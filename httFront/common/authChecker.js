53e70e5944229d6a320fac7bdfa9dbf64a8ef4e3
jpjphillips <jpphillips@uchicago.edu>
2021-08-25 13:31:06 -0500

    Moved front end assets to caMicroscope repo

--- httFront/common/authChecker.js
+++
@@ -1,9 +0,0 @@
-const __auth_check = function(levels){
-  fetch("../".repeat(levels) + "data/Template/find").then(x=>{
-    // send them to login
-    console.log(x)
-    if (x.status == 401){
-      window.location = "../".repeat(levels) + "login.html?state=" + encodeURIComponent(window.location)
-    }
-  })
-}
